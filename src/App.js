f682a199b80ddadfcb804969ae12b15f57915d70
Bonim <bonimouk@gmail.com>
2021-12-06 15:46:48 -0800

    Components, Props & PropTypes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Joh Doe';
-    const loading = true;
-    const showName = true;
-
     return (
       <div className='app'>
-        {!loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div>
     );
   }
