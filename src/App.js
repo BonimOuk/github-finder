8e6409edb8771fd807f1d60a72f436447683ca9b
Bonim <bonimouk@gmail.com>
2021-12-23 09:17:57 -0800

    Home & NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -22,18 +22,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Routes>
-                <Route
-                  exact
-                  path='/'
-                  element={
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  }
-                />
+                <Route exact path='/' element={<Home />} />
                 <Route exact path='/about' element={<About />} />
                 <Route path='/user/:login' element={<User />} />
+                <Route path='*' element={<NotFound />} />
               </Routes>
             </div>
           </div>
